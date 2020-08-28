944487a10ce7bd0014d84158c637056f06ea4a8b
Miguel Rojas <miguel@Miguels-MacBook-Air.local>
2020-08-28 13:13:59 -0600

    Creacion de Archivos de Clases

--- P3Lab6_MiguelRojas.cpp
+++ P3Lab6_MiguelRojas.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
 using namespace std;
+/*
+*************SALIDA ESPERADA*************
+x+y:
+-1
 
-int main(){
+w = z*y+x | (w-y)/z:
+-2
+
+Acumulando letras:  
+nw
+
+palabra*3: 
+nwnwnw
+
+Imprimir letra: 
+w
+*/
+int main(int argc, char const *argv[]){
+    STRING titulos;    
+    STRING palabra;    
+    STRING fin_linea;
+    CHAR letra1;
+    CHAR letra2;
+    INTEGER x;
+    INTEGER y;
+    INTEGER z;
+    INTEGER w;    
+
+    fin_linea<<"";
+
+    x<<1;
+    y<<-2;
+    z<<6;
+
+    titulos<<"x+y:";
+    !titulos;
+    !(x+y);
+    !fin_linea;
+
+    titulos<<"w = z*y+x | (w-y)/z:";
+    !titulos;
+    w << z*y+x;
+    !((w-x)/z);
+    !fin_linea;
+    
+    titulos<<"Acumulando letras: ";
+    !titulos;
+    letra1<<'n';
+    letra2<<'w';
+    !(letra1+letra2);
+    !fin_linea;
+
+    palabra << letra1+letra2;
+    
+    titulos<<"palabra*5: ";
+    !titulos;
+    !(palabra*5);
+    !fin_linea;
+
+    titulos<<"Imprimir letra: ";
+    !titulos;
+    letra1<<letra2;
+    !letra2;
+
     return 0;
 }
